dce6bad1f6283021acdd17006821972d1cae5f5c
hayashing <thiennguyen9804@gmail.com>
2025-05-24 10:15:36 +0700

    done send button

--- lib/presentation/home/newsfeed_screen/widget/comment_input.dart
+++ lib/presentation/home/newsfeed_screen/widget/comment_input.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:locket_clone/presentation/home/newsfeed_screen/newsfeed_screen_root.dart';
 
-class CommentInput extends StatelessWidget {
+class CommentInput extends StatefulWidget {
   final TextEditingController commentController;
   final VoidCallback commentHandler;
   const CommentInput({
@@ -10,8 +10,27 @@ class CommentInput extends StatelessWidget {
     required this.commentHandler,
   });
 
+  @override
+  State<CommentInput> createState() => _CommentInputState();
+}
+
+class _CommentInputState extends State<CommentInput> {
+  bool _canSend = false;
   Widget sendIcon() {
-    return Container();
+    return IconButton(
+      icon: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Color(0xffECF4F4).withAlpha(_canSend ? 255 : 153),
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          Icons.send_rounded,
+          color: Color(0xff738F81).withAlpha(_canSend ? 255 : 153),
+        ),
+      ),
+      onPressed: widget.commentHandler,
+    );
   }
 
   @override
@@ -19,27 +38,32 @@ class CommentInput extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(
         bottom: MediaQuery.of(context).viewInsets.bottom + 5,
+        left: 10,
+        right: 10,
       ),
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 12),
-        padding: EdgeInsets.symmetric(horizontal: 12),
-        decoration: BoxDecoration(
-          color: Color(0xffAAC2B3),
-          borderRadius: BorderRadius.circular(40),
-        ),
-        child: TextField(
-          autofocus: true,
-          cursorColor: Colors.white,
-          style: TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-            hintText: 'Nhập tin nhắn...',
-            hintStyle: TextStyle(color: Colors.white70),
-            border: InputBorder.none,
-            // suffix: sendIcon(),
+      child: TextField(
+        onChanged: (value) {
+          setState(() {
+            _canSend = value.isNotEmpty;
+          });
+        },
+        autofocus: true,
+        cursorColor: Colors.white,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Color(0xffAAC2B3),
+          hintText: 'Nhập tin nhắn...',
+          hintStyle: TextStyle(color: Colors.white70),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(900),
+            borderSide: BorderSide.none,
           ),
-
-          controller: commentController,
+          suffixIcon: sendIcon(),
+          // contentPadding: EdgeInsets.symmetric(vertical: 12),
         ),
+
+        controller: widget.commentController,
       ),
     );
   }
